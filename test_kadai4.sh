af80936e58098b867b152f215a7b457db970923e
SEKIGUCHI Tadashi <a2317ts@aiit.ac.jp>
2023-08-01 01:11:22 +0000

    ひな形の作成

---
+++ test_kadai4.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+
+# 入力の数が少ない場合
+# 入力なし
+# 入力１個
+# 入力３個以上
+
+# 正の自然数以外が入力された場合
+# a b -> "error: plz input natural number"
+# -1
+
+# 最大公約数が計算できているか
+# 2 4 -> 2
+echo "2" > /tmp/$$-ans
+./kadai4.sh 2 4 > /tmp/$$-result
+diff /tmp/$$-ans /tmp/$$-result || echo "テスト3‐1" >> /tmp/$$-error.log
+
+# 1000 100 -> 100
+# 12 8 -> 4
+
+if [ -f /tmp/$$-error.log ];then
+        cat /tmp/$$-error.log 1>&2
+        exit 1
+fi
+
+
+
