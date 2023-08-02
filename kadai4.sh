30f9fb6ce888f6f12369cde000402ae5487a14e8
Sekiguchi Tadashi <a2317ts@aiit.ac.jp>
2023-08-02 09:27:14 +0000

    exitする際は正常終了0、異常終了1の区分

--- kadai4.sh
+++ kadai4.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 import math
+import sys
+
 a = int(input('a ='))
 b = int(input('b ='))
 if a > 0 and b > 0:
     print(math.gcd(a, b))
+    sys.exit(0)
 else:
     print("error : plz input possitive natural number")
-
+    sys.exit(1)
